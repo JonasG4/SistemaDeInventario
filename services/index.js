ae941e630dde5867a7eb06652e3017a18c496136
josuecorea5 <josuepaiz487@gmail.com>
2022-04-09 12:54:17 -0600

    Start with CRUD price table

--- services/index.js
+++ services/index.js
@@ -1,5 +1,6 @@
 module.exports = {
     UsuarioService : require("./usuarioServices"),
     CategoriaService: require('./categoriaServices'),
-    ProductoService: require('./productoServices')
+    ProductoService: require('./productoServices'),
+    PrecioService: require('./precioServices')
 }
