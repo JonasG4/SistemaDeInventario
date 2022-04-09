2071a7ff3f590da5341553e54fe235d1cd7a03b8
josuecorea5 <josuepaiz487@gmail.com>
2022-04-09 00:14:31 -0600

    Start with price table

---
+++ dal/migrations/20220409045112-create-precios.js
@@ -0,0 +1,38 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('precios', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      product_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "productos",
+          key: "id"
+        }
+      },
+      precio: {
+        type: Sequelize.FLOAT,
+        min: 1
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('precios');
+  }
+};
--- dal/models/precios.js
+++ dal/models/precios.js
@@ -3,32 +3,26 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Productos extends Model {
+  class Precios extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      Productos.belongsTo(models.Categorias, {
-        foreignKey: 'cod_categoria'
-      })
-      Productos.hasMany(model.Precios, {
-        foreignKey: 'id'
+      Precios.belongsTo(models.Productos, {
+        foreignKey: 'product_id'
       })
     }
   }
-  Productos.init({
-    nom_producto: DataTypes.STRING,
-    tam_producto: DataTypes.STRING,
-    des_producto: DataTypes.TEXT,
-    cod_categoria: DataTypes.INTEGER,
+  Precios.init({
+    product_id: DataTypes.INTEGER,
+    precio: DataTypes.FLOAT,
     created_at: DataTypes.DATE,
     updated_at: DataTypes.DATE
   }, {
     sequelize,
-    timestamps: false,
-    modelName: 'Productos',
+    modelName: 'Precios',
   });
-  return Productos;
+  return Precios;
 };
